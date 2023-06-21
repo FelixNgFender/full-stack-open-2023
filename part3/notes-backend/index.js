b715d05d9258bbc0270bf6fb722d49e33cc96a49
felixng <ngthinh302@gmail.com>
2023-06-21 11:54:23 +0700

    part3-c: Error handling for notes backend

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -17,6 +17,14 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
 app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
@@ -28,11 +36,34 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
   const id = request.params.id;
-  Note.findById(id).then((note) => {
-    response.json(note);
-  });
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/notes", (request, response) => {
@@ -62,12 +93,15 @@ app.post("/api/notes", (request, response) => {
 
 app.delete("/api/notes/:id", (request, response) => {
   const id = request.params.id;
-  Note.findByIdAndRemove(id).then((result) => {
-    response.status(204).end();
-  });
+  Note.findByIdAndRemove(id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
