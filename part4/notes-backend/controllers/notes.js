b84280430a8a566be146015c2053ff28021eda2b
felixng <ngthinh302@gmail.com>
2023-06-22 17:35:14 +0700

    part4-c: Add user admin to notes

--- part4/notes-backend/controllers/notes.js
+++ part4/notes-backend/controllers/notes.js
@@ -1,21 +1,31 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", {
+    username: 1,
+    name: 1,
+  });
   response.json(notes);
 });
 
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user.id,
   });
 
   const savedNote = await note.save();
-  response.status(201).json(savedNote);
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
+  response.json(savedNote);
 });
 
 notesRouter.get("/:id", async (request, response) => {
--- part4/notes-backend/models/note.js
+++ part4/notes-backend/models/note.js
@@ -7,6 +7,7 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
+  user: String,
 });
 
 noteSchema.set("toJSON", {
