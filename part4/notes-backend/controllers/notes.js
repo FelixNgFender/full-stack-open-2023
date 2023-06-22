f60f96a14a88cf8f5687a03ff1944f7799975f92
felixng <ngthinh302@gmail.com>
2023-06-22 08:54:40 +0700

    part4-a: Restructure notes backend

---
+++ part4/notes-backend/controllers/notes.js
@@ -0,0 +1,70 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
+
+notesRouter.get("/", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+notesRouter.post("/", (request, response, next) => {
+  const body = request.body;
+
+  if (body.content === undefined) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.get("/:id", (request, response, next) => {
+  const id = request.params.id;
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+notesRouter.delete("/:id", (request, response, next) => {
+  const id = request.params.id;
+  Note.findByIdAndRemove(id)
+    // eslint-disable-next-line no-unused-vars
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.put("/:id", (request, response, next) => {
+  const { content, important } = request.body;
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = notesRouter;
